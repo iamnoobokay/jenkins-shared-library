66994fe33aaf04bdd0b8d4772117f7be5b732780
Pranav Poudyal <pranav.poudyal@grepsr.com>
2022-11-03 12:12:19 +0545

    class based implementation also added

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,9 @@
 #!usr/bin/env groovy
+
+// import Docker file
+import com.example.Docker
 def call(String imageName){
-     echo "building docker image"
-    withCredentials([usernamePassword(credentialsId:'dockerhub-login',passwordVariable: 'PASS',usernameVariable:'USER')]){
-        sh "docker build -t $imageName ."
-        sh "echo $PASS docker login -u $USER --password-stdin"
-        sh "docker push $imageName"
-        // pranavpo/my-repo:2.0
-    }
+    // create a new instance of class Docker, pass the context this, call method with imagename
+    // reason to pass this is to have various jenkins methods like withCredentials, usernamePassword etc available in the class 
+    return new Docker(this).buildDockerImage(imageName)
 }
