40f717cf9f7a1cb7d323d8bb74ce4f39ec27bc82
Pranav Poudyal <pranav.poudyal@grepsr.com>
2022-11-03 13:03:33 +0545

    try

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 #!usr/bin/env groovy
 
 // import Docker file
-import src.com.example.Docker
-
+import com.example.Docker
+// src/com.example/Docker.groovy
 def call(String imageName) {
     // create a new instance of class Docker, pass the context this, call method with imagename
     // reason to pass this is to have various jenkins methods like withCredentials etc available in the class 
