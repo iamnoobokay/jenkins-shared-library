e2c86557fa069a64ffab086ff5d6fbe916bac4e4
Pranav Poudyal <pranav.poudyal@grepsr.com>
2022-11-03 13:30:36 +0545

    changes reverted

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,18 @@
 #!usr/bin/env groovy
 
 // import Docker file
-import com.example.Docker
+// import com.example.Docker
 // src/com.example/Docker.groovy
 def call(String imageName) {
     // create a new instance of class Docker, pass the context this, call method with imagename
     // reason to pass this is to have various jenkins methods like withCredentials etc available in the class 
-    return new Docker(this).buildDockerImage(imageName)
+    // return new Docker(this).buildDockerImage(imageName)
+    // since import didnt work, revert
+    echo "building docker image"
+    withCredentials([usernamePassword(credentialsId:'dockerhub-login',passwordVariable: 'PASS',usernameVariable:'USER')]){
+        sh "docker build -t $imageName ."
+        sh "echo $PASS docker login -u $USER --password-stdin"
+        sh "docker push $imageName"
+        // pranavpo/my-repo:2.0
+    }
 }
