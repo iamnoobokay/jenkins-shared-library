d8b0130aac4041341ddbb18d894cacc9d8c4a67e
Pranav Poudyal <pranav.poudyal@grepsr.com>
2022-11-03 12:16:00 +0545

    class based implementation also added

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!usr/bin/env groovy
 
 // import Docker file
-import com.example.Docker
+import ../src/com.example.Docker
 def call(String imageName){
     // create a new instance of class Docker, pass the context this, call method with imagename
     // reason to pass this is to have various jenkins methods like withCredentials, usernamePassword etc available in the class 
